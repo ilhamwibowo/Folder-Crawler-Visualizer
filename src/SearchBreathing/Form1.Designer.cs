0af8011de2020bdcc8c0b66a77046543d575f5f6
AkmalArifin <13520037@std.stei.itb.ac.id>
2022-03-15 22:10:58 +0700

    Set input

--- src/SearchBreathing/Form1.Designer.cs
+++ src/SearchBreathing/Form1.Designer.cs
@@ -32,7 +32,11 @@
             this.buttonFolderBrowser = new System.Windows.Forms.Button();
             this.BFS = new System.Windows.Forms.RadioButton();
             this.DFS = new System.Windows.Forms.RadioButton();
-            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.inputFileName = new System.Windows.Forms.TextBox();
+            this.search = new System.Windows.Forms.Button();
+            this.checkBoxFindAll = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // labelTitle
@@ -47,7 +51,7 @@
             // 
             // buttonFolderBrowser
             // 
-            this.buttonFolderBrowser.Location = new System.Drawing.Point(442, 153);
+            this.buttonFolderBrowser.Location = new System.Drawing.Point(420, 157);
             this.buttonFolderBrowser.Name = "buttonFolderBrowser";
             this.buttonFolderBrowser.Size = new System.Drawing.Size(78, 31);
             this.buttonFolderBrowser.TabIndex = 1;
@@ -58,30 +62,77 @@
             // BFS
             // 
             this.BFS.AutoSize = true;
-            this.BFS.Location = new System.Drawing.Point(317, 309);
+            this.BFS.Location = new System.Drawing.Point(411, 222);
             this.BFS.Name = "BFS";
             this.BFS.Size = new System.Drawing.Size(45, 17);
             this.BFS.TabIndex = 2;
             this.BFS.TabStop = true;
             this.BFS.Text = "BFS";
             this.BFS.UseVisualStyleBackColor = true;
+            this.BFS.CheckedChanged += new System.EventHandler(this.BFS_CheckedChanged);
             // 
             // DFS
             // 
             this.DFS.AutoSize = true;
-            this.DFS.Location = new System.Drawing.Point(435, 309);
+            this.DFS.Location = new System.Drawing.Point(474, 222);
             this.DFS.Name = "DFS";
             this.DFS.Size = new System.Drawing.Size(46, 17);
             this.DFS.TabIndex = 3;
             this.DFS.TabStop = true;
             this.DFS.Text = "DFS";
             this.DFS.UseVisualStyleBackColor = true;
+            this.DFS.CheckedChanged += new System.EventHandler(this.DFS_CheckedChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // fbd
+            // 
+            this.fbd.SelectedPath = " ";
+            // 
+            // inputFileName
+            // 
+            this.inputFileName.Location = new System.Drawing.Point(411, 313);
+            this.inputFileName.Name = "inputFileName";
+            this.inputFileName.Size = new System.Drawing.Size(138, 20);
+            this.inputFileName.TabIndex = 4;
+            this.inputFileName.Text = "e.g. \"Tubes 2 Stima.pdf\"";
+            this.inputFileName.TextChanged += new System.EventHandler(this.inputFileName_TextChanged);
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(474, 398);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(75, 23);
+            this.search.TabIndex = 5;
+            this.search.Text = "Search";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // checkBoxFindAll
+            // 
+            this.checkBoxFindAll.AutoSize = true;
+            this.checkBoxFindAll.Location = new System.Drawing.Point(411, 272);
+            this.checkBoxFindAll.Name = "checkBoxFindAll";
+            this.checkBoxFindAll.Size = new System.Drawing.Size(113, 17);
+            this.checkBoxFindAll.TabIndex = 6;
+            this.checkBoxFindAll.Text = "Find all occurence";
+            this.checkBoxFindAll.UseVisualStyleBackColor = true;
+            this.checkBoxFindAll.CheckedChanged += new System.EventHandler(this.checkBoxFindAll_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1020, 547);
+            this.Controls.Add(this.checkBoxFindAll);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.inputFileName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.DFS);
             this.Controls.Add(this.BFS);
             this.Controls.Add(this.buttonFolderBrowser);
@@ -99,7 +150,11 @@
         private System.Windows.Forms.Button buttonFolderBrowser;
         private System.Windows.Forms.RadioButton BFS;
         private System.Windows.Forms.RadioButton DFS;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.TextBox inputFileName;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.CheckBox checkBoxFindAll;
     }
 }
 
--- src/SearchBreathing/Form1.cs
+++ src/SearchBreathing/Form1.cs
@@ -12,6 +12,11 @@ namespace SearchBreathing
 {
     public partial class Form1 : Form
     {
+        private string startingDirectory;
+        private string fileName;
+        private bool findAll;
+        private string searchType;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,10 +24,90 @@ namespace SearchBreathing
 
         private void buttonFolderBrowser_Click(object sender, EventArgs e)
         {
-            folderBrowserDialog.ShowDialog();
-
+            DialogResult result = fbd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                startingDirectory = fbd.SelectedPath;
+            }
         }
 
-        
+        private void BFS_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+
+            if (rb == null)
+            {
+                MessageBox.Show("Sender is not a RadioButton");
+                return;
+            }
+
+            if (rb.Checked)
+            {
+                searchType = "BFS";
+            }
+        }
+        private void DFS_CheckedChanged_1(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+
+            if (rb == null)
+            {
+                MessageBox.Show("Sender is not a RadioButton");
+                return;
+            }
+
+            if (rb.Checked)
+            {
+                searchType = "DFS";
+            }
+        }
+
+        private void inputFileName_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            if (tb == null)
+            {
+                MessageBox.Show("Sender is not a Text Box");
+                return;
+            }
+            else
+            {
+                fileName = tb.Text;
+            }
+        }
+        private void checkBoxFindAll_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb == null)
+            {
+                MessageBox.Show("Sender is not a Check Box");
+            }
+            else
+            {
+                findAll = cb.Checked;
+            }
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            // call bfs or dfs function
+            if (searchType == "BFS")
+            {
+                // call bfs function, with params
+                // startingDirectory    - directory awal
+                // fileName             - nama file yang dicar
+                // findAll              - bool, if true cari semuanya
+            }
+            else if (searchType == "DFS")
+            {
+                // call dfs function, with params
+                // startingDirectory    - directory awal
+                // fileName             - nama file yang dicar
+                // findAll              - bool, if true cari semuany
+            }
+        }
+
     }
 }
