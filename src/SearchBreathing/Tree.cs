9d24e07a145d93200b70c623ec766d86af7aba7d
AkmalArifin <13520037@std.stei.itb.ac.id>
2022-03-19 06:12:36 +0700

    Add Tree Class

---
+++ src/SearchBreathing/Tree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchBreathing
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> root { get; set; }
+    }
+}
